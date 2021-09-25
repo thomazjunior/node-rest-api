157072f7d8d8f400f42cf627124be41c27b3f2fe
Paulo Thomaz Junior <p.thomaz.junior@gmail.com>
2021-09-25 01:35:34 -0300

    Initilizing node-rest-api

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL, {useNewUrlParser: true}, ()=>{
+    console.log("Connected to MongoDB")
+});
+
+//middleware
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+
+app.get("/",(req, res)=>{
+    res.send("Welcome to homepage")
+})
+
+app.get("/users",(req, res)=>{
+    res.send("Welcome to user page")
+})
+
+app.listen(8800,()=>{
+    console.log("Backend server is runing!")
+})
