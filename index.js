4b2109f67412d33760fe8fce87385c7e22a09ac2
Paulo Thomaz Junior <p.thomaz.junior@gmail.com>
2021-09-25 02:03:21 -0300

    Routers implemented

--- index.js
+++ index.js
@@ -4,12 +4,13 @@ const dotenv = require("dotenv");
 const helmet = require("helmet");
 const morgan = require("morgan");
 const mongoose = require("mongoose");
-
+const userRoute = require("./routes/users");
+const authRoute = require("./routes/auth");
 
 dotenv.config();
 
-mongoose.connect(process.env.MONGO_URL, {useNewUrlParser: true}, ()=>{
-    console.log("Connected to MongoDB")
+mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true }, () => {
+  console.log("Connected to MongoDB");
 });
 
 //middleware
@@ -17,14 +18,18 @@ app.use(express.json());
 app.use(helmet());
 app.use(morgan("common"));
 
-app.get("/",(req, res)=>{
-    res.send("Welcome to homepage")
-})
+app.use("/api/users", userRoute);
+app.use("/api/auth", authRoute);
 
-app.get("/users",(req, res)=>{
-    res.send("Welcome to user page")
-})
 
-app.listen(8800,()=>{
-    console.log("Backend server is runing!")
-})
+app.get("/", (req, res) => {
+  res.send("Welcome to homepage");
+});
+
+app.get("/users", (req, res) => {
+  res.send("Welcome to user page");
+});
+
+app.listen(8800, () => {
+  console.log("Backend server is runing!");
+});
